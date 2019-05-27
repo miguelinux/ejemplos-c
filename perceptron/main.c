1c057d758c70371cbdc4e2841b3f04ca547e6987
Miguel Bernal Marin <miguel.bernal.marin@linux.intel.com>
2019-05-27 17:47:22 -0500

    add definitions

--- perceptron/main.c
+++ perceptron/main.c
@@ -4,9 +4,23 @@
 
 #define MAX 10
 
-double x[MAX];
+double x[MAX]; /* x es vector de las entradas */
+double w[MAX]; /* w es vector de peso */
+double y;      /* y es la salida de la neurona */
+double delta;  /* delta es la salida esperada */
+const double alpha = 0.5;  /* alpha es una constante tal que 0 < alpha < 1 */
+
+int con_tasa = 1 /* Con tasa de aprendizaje 1 = verdadero, 0 = sin tasa de aprendizaje */
 
 int main (int argc, char *argv[])
 {
+
+	if (con_tasa) {
+		w[j] = w[j] + alpha * (delta - y) * x[j]
+	} else {
+		w[j] = w[j] +(delta - y) * x[j]
+	}
+
+
 	return 0;
 }
