f1ecd102f97a8f92dadd72e4aa373371d92e00c9
Miguel Bernal Marin <miguel.bernal.marin@linux.intel.com>
2019-06-03 18:01:30 -0500

    add stdlib and time
    move to AND function the if structure

--- perceptron/main.c
+++ perceptron/main.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 /* Basado de: https://es.wikipedia.org/wiki/Perceptr%C3%B3n */
 
@@ -6,8 +8,8 @@
 #define MAX 10
 
 double x[PUNTO][MAX]; /* x es vector de las entradas */
-double w[PUNTO][MAX]; /* w es vector de peso */
-double y;      /* y es la salida de la neurona */
+double w[MAX]; /* w es vector de peso */
+double y[MAX];      /* y es la salida de la neurona */
 double delta[MAX];  /* delta es la salida esperada */
 const double alpha = 0.5;  /* alpha es una constante tal que 0 < alpha < 1 */
 
@@ -33,16 +35,24 @@ void ejemplo_AND() {
 	    x[3][1]=0;
 	delta[3]=0;
 
-}
+	int entradas = 2;
+	int muestras = 4;
 
-int main (int argc, char *argv[])
-{
+	w[0] = 1/rand();
+	w[1] = 1/rand();
 
 	if (con_tasa) {
 		w[j] = w[j] + alpha * (delta - y) * x[j];
 	} else {
 		w[j] = w[j] +(delta - y) * x[j];
 	}
+
+}
+
+int main (int argc, char *argv[])
+{
+
+	srand(time(NULL));
 
 
 	return 0;
